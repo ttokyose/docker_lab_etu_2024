43e71290043d040fb5eee5a414fb373cf3e61b2e
Lucr3tia <112802802+Lucr3tia@users.noreply.github.com>
2024-12-24 20:14:31 +0300

    Delete To-Do-List-Flask-main/Todo List directory

--- To-Do-List-Flask-main/Todo List/models.py
+++
@@ -1,7 +0,0 @@
-from app import db
-
-class Todo(db.Model):
-    sr_no = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    desc = db.Column(db.String(500), nullable=False)
-    date_created = db.Column(db.DateTime, default=db.func.now())
